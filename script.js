5cc10b5353099bb8c91d9baab14b4f1e6029d878
Tara <wisairet@gmail.com>
2026-07-01 21:39:02 +0330

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ window.addEventListener("load", () => {
   const intro = document.getElementById("intro");
   const site = document.getElementById("site");
 
-  // 🌸 بعد از 3 ثانیه Intro محو میشه
+  // 🌸 بعد از 3 ثانیه intro محو میشه
   setTimeout(() => {
+
     intro.style.opacity = "0";
 
-    // بعد از محو شدن کامل
+    // بعد از کامل fade شدن
     setTimeout(() => {
       intro.style.display = "none";
+
+      // سایت رو نشون بده
       site.style.display = "block";
 
-      // ورود نرم سایت
-      site.style.opacity = "0";
-      site.style.transition = "opacity 1s ease";
-      
+      // ورود نرم
       setTimeout(() => {
         site.style.opacity = "1";
       }, 50);
@@ -26,14 +26,15 @@ window.addEventListener("load", () => {
 
 });
 
+
 // 🌸 Explore button animation
 function explore() {
   const hero = document.querySelector(".hero");
 
-  hero.style.transform = "scale(0.95)";
-  hero.style.transition = "0.3s";
+  hero.style.transform = "scale(0.96)";
+  hero.style.transition = "0.25s";
 
   setTimeout(() => {
     hero.style.transform = "scale(1)";
-  }, 300);
+  }, 250);
 }
